f86d6d2f304f2e42d91ebe6bd3509b65ee93e666
Chris Ridings <chris@chrisridings.com>
2026-03-02 14:24:46 +0100

    update tests

--- runtests.php
+++ runtests.php
@@ -8,14 +8,20 @@ $llvmAsPath = 'C:/Program Files/LLVM/bin/llvm-as.exe';
 $clangPath = 'C:/Program Files/LLVM/bin/clang.exe';
 $testDir = 'tests';
 
-// Test files in order
-$testFiles = [
-    '1.php',
-    '2.php',
-    '3.php',
-    '4.php',
-    '5.php'
-];
+// Get all test files dynamically
+$testFiles = [];
+$dirHandle = opendir($testDir);
+if ($dirHandle) {
+    while (($file = readdir($dirHandle)) !== false) {
+        if (preg_match('/^\d+\.php$/', $file)) {
+            $testFiles[] = $file;
+        }
+    }
+    closedir($dirHandle);
+    // Sort the test files numerically
+    natsort($testFiles);
+    $testFiles = array_values($testFiles);
+}
 
 $passCount = 0;
 $failCount = 0;
---
+++ tests/6.php
@@ -0,0 +1,13 @@
+<?php
+
+$x = 10;
+
+if ($x > 5) {
+    echo "big\n";
+} else {
+    echo "small\n";
+}
+
+if ($x > 8) {
+    echo "more than 8!\n";
+}
