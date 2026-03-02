88641baf84a9cad13e65efd9b4e8cde3e045d624
Chris Ridings <chris@chrisridings.com>
2026-03-02 10:17:23 +0100

    fix typo in compile

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 php llvm/run.php helloworld.php
-C:\Program Files\LLVM\bin\llvm-as" output.ll -o output.bc
+"C:\Program Files\LLVM\bin\llvm-as" output.ll -o output.bc
 "c:\program files\llvm\bin\clang.exe" -c libphp\stdlib.c -o stdlib.obj
 "c:\program files\llvm\bin\clang.exe" -c output.bc -o output.obj
 "c:\program files\llvm\bin\clang.exe" output.obj stdlib.obj -o hello.exe
